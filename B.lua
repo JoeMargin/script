683cff50663b69c729bfce9acd75cfcb36e8d335
JoeMargin <159750954+JoeMargin@users.noreply.github.com>
2024-06-25 19:08:37 +0800

    Update B.lua
    added zero velocity

--- B.lua
+++ B.lua
@@ -48,6 +48,7 @@ function createtool(_)
 							count = 0
 							goback = false
 						end
+						localplayer.Character:FindFirstChild("Humanoid").RootPart.Velocity = Vector3.zero
 						localplayer.Character:FindFirstChild("Humanoid").RootPart.CFrame = target:FindFirstChild("Humanoid").RootPart.CFrame * CFrame.new(0,0,1) * CFrame.Angles(math.rad((-30 * count) + 10),0,0)
 					else
 						break
